5a00e24f162c4ae7a07aa623a83082414cc01146
1348858_BALAJI <balajimadhavan95@gmail.com>
2024-11-30 19:13:33 +0530

    whatsapp-icon added

--- src/Main/ContactUs.js
+++ src/Main/ContactUs.js
@@ -4,11 +4,11 @@ function ContactUs(){
         <div className="contactUs">
         <p>Contact Us</p>
         <i class="bi bi-person">Balaji M</i>
-        <i class="bi bi-telephone">+91-987654321</i>
+        <i class="bi bi-whatsapp">+91-987654321</i>
         <i class="bi bi-envelope">balajimadhavan95@gmail.com </i>
 
         <i class="bi bi-person">Sri Karthikeyan</i>
-        <i class="bi bi-telephone">+91-987654312</i>
+        <i class="bi bi-whatsapp">+91-987654312</i>
         <i class="bi bi-envelope"> srikarthikeyan212001@gmail.com</i>
         </div>
     )
