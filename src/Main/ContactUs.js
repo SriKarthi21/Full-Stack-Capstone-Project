4b41eab25716cadb7badf3d977789a762334de03
Srikarthikeyan <158450516+SriKarthi21@users.noreply.github.com>
2025-02-03 13:46:41 +0530

    Update ContactUs.js

--- src/Main/ContactUs.js
+++ src/Main/ContactUs.js
@@ -2,11 +2,6 @@ function ContactUs(){
 
     return(
         <div className="contactUs">
-        <p style={{textDecoration:"underline"}}>Contact Us</p>
-        <i class="bi bi-person">Balaji M</i>
-        <i class="bi bi-whatsapp">+91-987654321</i>
-        <i class="bi bi-envelope">balajimadhavan95@gmail.com </i>
-<p></p>
         <i class="bi bi-person">Sri Karthikeyan</i>
         <i class="bi bi-whatsapp">+91-8300732477</i>
         <i class="bi bi-envelope"> srikarthikeyan212001@gmail.com</i>
